5e5485a3f79bdced9682a76261b7da4c7b1615e4
saka <asclepius1993@gmail.com>
2017-07-18 12:09:26 -0400

    write setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+    name = 'zillowAPI',
+    version = '0.1.0',
+    description = 'a zillow.com API wrapper written in python',
+    author = 'Cong Du',
+    author_email = 'asclepius1993@gmail.com',
+    keywords=['zillow','api','real estate'],
+    packages = ['zillowAPI'],
+    url='https://github.com/asclepiusaka/zillowAPI',
+    download_url = 'https://github.com/asclepiusaka/zillowAPI/archive/0.1.0.tar.gz',
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers'
+    ]
+)
